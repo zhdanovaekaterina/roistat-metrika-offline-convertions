0409fed5e8aeaec589d689ae5354a504ab0e7a99
e.nasedkina <zhdanova.ekaterina.95@yandex.ru>
2022-06-09 18:02:26 +0300

    ver 0.2

---
+++ config_template.py
@@ -0,0 +1,7 @@
+roistat_token = ''
+roistat_api_url = 'https://cloud.roistat.com/api/v1'
+roistat_project = ''
+
+metrika_token = ''
+counter_1 = ''
+counter_2 = ''
--- script.py
+++ script.py
@@ -6,11 +6,10 @@ import pprint
 import json
 import time
 import pandas as pd
+from tapi_yandex_metrika import YandexMetrikaStats
 
 start_time = time.time()
 
-url = 'https://cloud.roistat.com/api/v1'
-client_project = '72370'
 request_headers = {'Api-key': config.roistat_token,
     'Content-type': 'application/json'
     }
@@ -19,12 +18,13 @@ start_date = '2022-05-30T00:00:00+0000'
 end_date = '2022-06-01T00:00:00+0000'
 limit = 100
 
+
 def get_all_statuses():
     '''Возвращает словарь всех статусов.'''
     api = '/project/integration/status/list'
-    full_url = url + api
+    full_url = config.roistat_api_url + api
 
-    params = {'project': client_project}
+    params = {'project': config.roistat_project}
 
     r = req.post(full_url, headers=request_headers, params=params)
     data = json.loads(r.text)
@@ -51,10 +51,10 @@ def get_target_statuses(all_statuses:dict):
 def get_all_deals(start_date:str, end_date:str):
     '''Принимает дату начала и дату окончания периода в формате UTC0. Возвращает сделки за указанный период.'''
     api = '/project/integration/order/list'
-    full_url = url + api
+    full_url = config.roistat_api_url + api
 
     params = {
-        'project': client_project,
+        'project': config.roistat_project,
         'limit': limit
     }
 
@@ -86,9 +86,58 @@ def get_roistat_target_deals(data):
     roistat_data = pd.DataFrame(deals)
     return roistat_data
 
+def get_metrika_data(counter, file_name):
+    '''Принимает ID счетчика и имя файла, в который запишет результат (необходим на этапе проверки данных). Получает данные из Яндекс Метрики.'''
+    ACCESS_TOKEN = config.metrika_token
+    METRIC_IDS = counter
+
+    api = YandexMetrikaStats(access_token=ACCESS_TOKEN, receive_all_data=True)
+
+    params = dict(
+    ids = METRIC_IDS,
+    metrics = "ym:s:users,ym:s:visits",
+    dimensions = "ym:s:date,ym:s:visitID,ym:s:paramsLevel1",    # ym:s:paramsLevel1 передает название, а не значение параметра
+    date1 = "2daysAgo",
+    date2 = "yesterday",
+    sort = "ym:s:date",
+    accuracy="full",
+    limit = 1000)
+
+    result = api.stats().get(params=params)
+    result = result().data
+    result = result[0]['data']
+
+    dict_data = {}
+
+    for i in range(0, len(result)-1):
+        dict_data[i] = {
+            'date':result[i]["dimensions"][0]["name"],
+            'metrika-id':result[i]["dimensions"][1]["name"],
+            'roistat-visit-id':result[i]["dimensions"][2]["name"],
+            'users':result[i]["metrics"][0],
+            'visits':result[i]["metrics"][1]
+        }
+
+    dict_keys = dict_data[0].keys()
+    df = pd.DataFrame.from_dict(dict_data, orient='index',columns=dict_keys)
+
+    # Выгрузка данных в эксель для проверки
+    df.to_excel(f"{file_name}.xlsx",
+        sheet_name='data',
+        index=False)
+
+    return(df)
+
+
 def main():
-    data = get_all_deals(start_date, end_date)
-    roistat_data = get_roistat_target_deals(data)
+    # data = get_all_deals(start_date, end_date)
+    # roistat_data = get_roistat_target_deals(data)
+    # print(roistat_data)
+    metrika_data_1 = get_metrika_data(config.counter_1, 'okno_ru')
+    # print(metrika_data_1.head(10))
+    metrika_data_2 = get_metrika_data(config.counter_2, 'okno_moskva_ru')
+    # print(metrika_data_2.head(10))
+
 
     end_time = time.time()
     total_time = end_time - start_time
